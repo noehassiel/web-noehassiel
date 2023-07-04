b044ad26eda3245d134254379c31790595f7f00f
noehassiel <hassiel-monterrosas@hotmail.com>
2023-07-03 19:26:46 -0600

    angular routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AngularController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
